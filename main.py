9207c8b08c4b459b3648c7ee5e780a3f42d61960
Zhony6470 <gomezanthonydavid@gmail.com>
2026-08-27 15:40:08 -0400

    Support health check HEAD requests

--- main.py
+++ main.py
@@ -747,6 +747,10 @@ class Bot(BaseBot):
                 self.end_headers()
                 self.wfile.write(b"OK")
 
+            def do_HEAD(self):
+                self.send_response(200)
+                self.end_headers()
+
             def log_message(self, format, *args):
                 return
 
